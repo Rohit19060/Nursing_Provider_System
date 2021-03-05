6286732ddbbb0ff591161e13fa2539fb1b850007
Rohit19060 <rohitjain19060@gmail.com>
2021-03-05 19:48:13 +0530

    Feedback File Added

--- cancelHire.php
+++ cancelHire.php
@@ -8,7 +8,7 @@ $h_id = $_GET['id'];
 require_once("./includes/db.php");
 $query_update = "UPDATE `freelancer` SET `order_id`='0',`hired`='0' WHERE `h_id` = $h_id";
 if (mysqli_query($connection, $query_update)) {
-    $_SESSION["message"] = "Hiring Canceled, Refund Initiated";
+    $_SESSION["message"] = "Hiring cancel, Refund Initiated and you will be notified within 24 hours";
     redirect();
 } else {
     $_SESSION["message"] = "There is some error";
---
+++ feedback.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h2>Feedback Send</h2>";
+echo "<a href='index.php' class='inputDiv text-center mb-2'><button>Go Back</button></a>";
+?>
